75e10af9c44596a1aef25f79acf448ca3efd2457
Sung Joon, Lee <wcmskorea@gmail.com>
2018-05-14 17:46:28 +0900

    11장까지...

--- database/migrations/2018_05_14_083703_create_authors_table.php
+++ database/migrations/2018_05_14_083703_create_authors_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateAuthorsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,8 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		//
+		Schema::create('authors', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('name');
@@ -30,7 +31,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		//
 	}
 
 }
--- database/migrations/2018_05_14_084037_add_name_to_authors_table.php
+++ database/migrations/2018_05_14_084037_add_name_to_authors_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration {
+class AddNameToAuthorsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,11 +12,9 @@ class CreatePasswordResetsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('password_resets', function(Blueprint $table)
-		{
-			$table->string('email')->index();
-			$table->string('token')->index();
-			$table->timestamp('created_at');
+		//
+		Schema::table('authors', function(Blueprint $table) {
+			$table->string('profile')->after('password')->nullable();
 		});
 	}
 
@@ -27,7 +25,10 @@ class CreatePasswordResetsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('password_resets');
+		//
+		Schema::table('authors', function(Blueprint $table) {
+			$table->dropColumn('profile');
+		});
 	}
 
 }
