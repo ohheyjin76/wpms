24a9a88c0f23605f0d5f06daf8e71cc4f3bdaff6
Sung Joon, Lee <wcmskorea@gmail.com>
2018-05-17 16:55:50 +0900

    admin-lte update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,87 +1,44 @@
-<!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>{{ $page_title or "AdminLTE Dashboard" }}</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <!-- Bootstrap 3.3.2 -->
-    <link href="/bower/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="/bower/font-awesome/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="/bower/Ionicons/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link href="/bower/admin-lte/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
-    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-          page. However, you can choose any other skin. Make sure you
-          apply the skin class to the body tag so the changes take effect.
-    -->
-    <link href="/bower/admin-lte/dist/css/skins/skin-blue.min.css" rel="stylesheet" type="text/css" />
+@extends('admin')
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body class="hold-transition skin-blue sidebar-mini">
-    <div class="wrapper">
+@section('content')
+<div class="wrapper">
 
-      <!-- Header -->
-      @include('admin.adminHeader')
+  <!-- Header -->
+  @include('admin.adminHeader')
 
-      <!-- Sidebar -->
-      @include('admin.adminSidebar')
+  <!-- Sidebar -->
+  @include('admin.adminSidebar')
 
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
 
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            {{ $page_title or "Page Title" }}
-            <small>{{ $page_description or null }}</small>
-          </h1>
-          <!-- You can dynamically generate breadcrumbs here -->
-          <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-            <li class="active">Here</li>
-          </ol>
-        </section>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        {{ $page_title or "Page Title" }}
+        <small>{{ $page_description or null }}</small>
+      </h1>
+      <!-- You can dynamically generate breadcrumbs here -->
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
 
-        <!-- Main content -->
-        <section class="content">          
-          <!-- Your Page Content Here -->
-          @yield('content')
-        </section><!-- /.content -->
-      </div><!-- /.content-wrapper -->
+    <!-- Main content -->
+    <section class="content">          
+      <!-- Your Page Content Here -->
+      @yield('content')
+    </section><!-- /.content -->
+  </div><!-- /.content-wrapper -->
 
-      <!-- Footer -->
-      @include('admin.adminFooter')
+  <!-- Footer -->
+  @include('admin.adminFooter')
 
-      <!-- Side Panel -->
-      @include('admin.adminSidepanel')
+  <!-- Side Panel -->
+  @include('admin.adminSidepanel')
 
-      <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
-      <div class="control-sidebar-bg"></div>
-    </div><!-- ./wrapper -->
-
-    <!-- REQUIRED JS SCRIPTS -->
-
-    <!-- jQuery 3 -->
-    <script src="bower/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap 3.3.7 -->
-    <script src="bower/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="bower/admin-lte/dist/js/adminlte.min.js"></script>
-
-    <!-- Optionally, you can add Slimscroll and FastClick plugins. 
-          Both of these plugins are recommended to enhance the 
-          user experience -->
-  </body>
-</html>
+  <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div><!-- ./wrapper -->
+@endsection
